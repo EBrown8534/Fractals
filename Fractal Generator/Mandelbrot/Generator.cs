55d6d8da4eba026f43d33646c82a017bcf8b8fa4
EBrown8534 <ebrown8534@gmail.com>
2015-10-12 15:16:30 -0400

    Extracted Mandelbrot generation out of `Program`. Added logging to Mandelbrot generation.

--- Fractal Generator/Mandelbrot/Generator.cs
+++ Fractal Generator/Mandelbrot/Generator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Evbpc.Framework.Utilities.Logging;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -11,6 +12,8 @@ namespace Fractal_Generator.Mandelbrot
     {
         public const double twoSquared = 2 * 2;
 
+        public ILogger Logger { get; }
+
         public int NumberOfCores { get; set; }
         public int NumberOfChunks { get; set; }
         public ushort MaxIterations { get; set; }
@@ -20,7 +23,8 @@ namespace Fractal_Generator.Mandelbrot
 
         private readonly Func<int, int, int> pointToIndex;
 
-        public Generator(int numberOfCores,
+        public Generator(ILogger logger,
+                         int numberOfCores,
                          int numberOfChunks,
                          ushort maxIterations,
                          Size imageSize,
@@ -33,12 +37,14 @@ namespace Fractal_Generator.Mandelbrot
             ImageSize = imageSize;
             NumberOfCores = numberOfCores;
             BlackFinalColor = blackFinalColor;
+            Logger = logger;
 
             pointToIndex = delegate (int x, int y) { return y * ImageSize.Width + x; };
         }
 
         public Color[] BuildColors()
         {
+            Logger?.LogInformation($"Building colors...");
             Color[] colors = new Color[NumberOfColors];
 
             for (int i = 0; i < NumberOfColors - 1; i++)
@@ -51,25 +57,166 @@ namespace Fractal_Generator.Mandelbrot
                 colors[NumberOfColors - 1] = Color.FromArgb(255, 0, 0, 0);
             }
 
+            Logger?.LogInformation($"Colors built, returning.");
             return colors;
         }
 
-        public Image BuildImage(ushort[] results)
+        public Bitmap BuildImage(ushort[] results)
         {
+            Logger?.LogInformation($"Building image...");
             Color[] colors = BuildColors();
 
-            using (Bitmap image = new Bitmap(ImageSize.Width, ImageSize.Height))
+            Bitmap image = new Bitmap(ImageSize.Width, ImageSize.Height);
+
+            for (int y = 0; y < ImageSize.Height; y++)
             {
-                for (int y = 0; y < ImageSize.Height; y++)
+                for (int x = 0; x < ImageSize.Width; x++)
                 {
-                    for (int x = 0; x < ImageSize.Width; x++)
+                    image.SetPixel(x, y, colors[results[pointToIndex(x, y)] / (int)(Math.Ceiling(MaxIterations / (double)NumberOfColors))]);
+                }
+            }
+
+            Logger?.LogInformation($"Image built, returning.");
+            return image;
+        }
+
+        public ushort[] Generate(Point center, SizeF scaleSize)
+        {
+            Logger?.LogInformation($"Building {NumberOfChunks} chunks...");
+            // Build our chunks.
+            List<Chunk> chunks = new List<Chunk>(NumberOfChunks);
+            for (int i = 0; i < NumberOfChunks; i++)
+            {
+                chunks.Add(new Chunk(new Point(0, ImageSize.Height / NumberOfChunks * i), new Point(ImageSize.Width, ImageSize.Height / NumberOfChunks * (i + 1))));
+            }
+
+            Logger?.LogInformation($"Build chunks.");
+            Logger?.LogInformation($"Creating and assigning chunks, this may take a while...");
+
+            // Create and assign tasks (as we can).
+            List<Task<Result>> tasks = new List<Task<Result>>();
+            while (chunks.Count > 0)
+            {
+                if (tasks.Where(x => x.Status != TaskStatus.RanToCompletion).ToList().Count < NumberOfCores)
+                {
+                    if (chunks.Count > 0)
                     {
-                        image.SetPixel(x, y, colors[results[pointToIndex(x, y)] / (int)(Math.Ceiling(MaxIterations / (double)NumberOfColors))]);
+                        Logger?.LogInformation($"Assigning chunk...");
+                        Task<Result> getSection = GenerateSectionAsync(Logger, chunks[0], center, scaleSize, MaxIterations);
+                        chunks.Remove(chunks[0]);
+                        tasks.Add(getSection);
                     }
                 }
 
-                return image;
+                System.Threading.Thread.Sleep(1);
             }
+
+            Logger?.LogInformation($"Last chunk assigned, waiting for results.");
+
+            // Create the main results
+            ushort[] results = new ushort[ImageSize.Width * ImageSize.Height];
+
+            Func<int, int, int> pointToIndex = delegate (int x, int y) { return y * ImageSize.Width + x; };
+
+            // Make sure we finish our tasks and add them to our results.
+            while (tasks.Count > 0)
+            {
+                var finishedTasks = tasks.Where(x => x.Status == TaskStatus.RanToCompletion).ToList();
+
+                foreach (var finishedTask in finishedTasks)
+                {
+                    Result result = finishedTask.Result;
+
+                    for (int y = result.Chunk.Start.Y; y < result.Chunk.End.Y; y++)
+                    {
+                        for (int x = 0; x < ImageSize.Width; x++)
+                        {
+                            results[pointToIndex(x, y)] = result.Data[pointToIndex(x, (y - result.Chunk.Start.Y))];
+                        }
+                    }
+
+                    tasks.Remove(finishedTask);
+                }
+
+                System.Threading.Thread.Sleep(1);
+            }
+            
+            Logger?.LogInformation($"Results computed.");
+
+            return results;
+        }
+
+        public static Task<Result> GenerateSectionAsync(ILogger logger, Chunk chunk, Point center, SizeF scaleSize, ushort maxIterations)
+        {
+            return Task.Run(() =>
+            {
+                return GenerateSection(logger, chunk, center, scaleSize, maxIterations);
+            });
+        }
+
+        private static Result GenerateSection(ILogger logger, Chunk chunk, Point center, SizeF scaleSize, ushort maxIterations)
+        {
+            logger?.LogInformation($"Generating section {chunk.Start.ToString()}:{chunk.End.ToString()}.");
+            int startRow = chunk.Start.Y;
+            int endRow = chunk.End.Y;
+
+            int startColumn = chunk.Start.X;
+            int endColumn = chunk.End.X;
+
+            int height = endRow - startRow;
+            int width = endColumn - startColumn;
+
+            ushort[] results = new ushort[height * width];
+
+            // We'll need all of these later.
+            int relativeRow = 0;
+            ushort iteration = 0;
+            double xTemp = 0;
+            double x0 = 0f;
+            double y0 = 0f;
+            double xL = 0f;
+            double yL = 0f;
+            double xSquared = 0;
+            double ySquared = 0;
+
+            double negCenterX = -center.X;
+            double negCenterY = -center.Y;
+            double reciprocalScaleSizeWidth = 1 / scaleSize.Width;
+            double reciprocalScaleSizeHeight = 1 / scaleSize.Height;
+
+            for (int y = startRow; y < endRow; y++)
+            {
+                for (int x = startColumn; x < endColumn; x++)
+                {
+                    // The formula for a mandelbrot is z = z^2 + c, basically. We must relate that in code.
+                    x0 = (x + negCenterX) * reciprocalScaleSizeWidth;
+                    y0 = (y + negCenterY) * reciprocalScaleSizeHeight;
+                    xL = 0;
+                    yL = 0;
+
+                    iteration = 0;
+                    xSquared = xL * xL;
+                    ySquared = yL * yL;
+                    xTemp = 0;
+
+                    while (xSquared + ySquared < twoSquared && iteration < maxIterations)
+                    {
+                        xTemp = xSquared - ySquared + x0;
+                        yL = 2 * xL * yL + y0;
+                        xL = xTemp;
+                        iteration++;
+                        xSquared = xL * xL;
+                        ySquared = yL * yL;
+                    }
+
+                    results[relativeRow * width + x] = iteration;
+                }
+
+                relativeRow++;
+            }
+
+            logger?.LogInformation($"Section {chunk.Start.ToString()}:{chunk.End.ToString()} generated, returning.");
+            return new Result(chunk, results);
         }
     }
 }
--- Fractal Generator/Program.cs
+++ Fractal Generator/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -80,92 +81,28 @@ namespace Fractal_Generator
 
             Console.WriteLine($"Creating Mandelbrot image of size ({imageSize.Width},{imageSize.Height}) and max iteration count of {maxIterations}, splitting the image into {numberOfChunks} sections across {numberOfCores} cores.");
 
+            bool blackFinalColor = false;
+
+            if (prompt.Prompt("Use black for max iteration color?", PromptOptions.Optional, 'Y', null, validateYesNo, charToUpper) == 'Y')
+            {
+                blackFinalColor = true;
+            }
+
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            // Build our chunks.
-            List<Chunk> chunks = new List<Chunk>(numberOfChunks);
-            for (int i = 0; i < numberOfChunks; i++)
-            {
-                chunks.Add(new Chunk(new Point(0, imageSize.Height / numberOfChunks * i), new Point(imageSize.Width, imageSize.Height / numberOfChunks * (i + 1))));
-            }
-
-            // Create and assign tasks (as we can).
-            List<Task<Result>> tasks = new List<Task<Result>>();
-            while (chunks.Count > 0)
-            {
-                if (tasks.Where(x => x.Status != TaskStatus.RanToCompletion).ToList().Count < numberOfCores)
-                {
-                    if (chunks.Count > 0)
-                    {
-                        Task<Result> getSection = GenerateSectionAsync(chunks[0], center, scaleSize, maxIterations);
-                        chunks.Remove(chunks[0]);
-                        tasks.Add(getSection);
-                    }
-                }
-
-                System.Threading.Thread.Sleep(1);
-            }
-
-            Console.WriteLine("Last chunk assigned, waiting for results.");
-
-            // Create the main results
-            ushort[] results = new ushort[imageSize.Width * imageSize.Height];
-
-            Func<int, int, int> pointToIndex = delegate (int x, int y) { return y * imageSize.Width + x; };
-
-            // Make sure we finish our tasks and add them to our results.
-            while (tasks.Count > 0)
-            {
-                var finishedTasks = tasks.Where(x => x.Status == TaskStatus.RanToCompletion).ToList();
-
-                foreach (var finishedTask in finishedTasks)
-                {
-                    Result result = finishedTask.Result;
-
-                    for (int y = result.Chunk.Start.Y; y < result.Chunk.End.Y; y++)
-                    {
-                        for (int x = 0; x < imageSize.Width; x++)
-                        {
-                            results[pointToIndex(x, y)] = result.Data[pointToIndex(x, (y - result.Chunk.Start.Y))];
-                        }
-                    }
-
-                    tasks.Remove(finishedTask);
-                }
-
-                System.Threading.Thread.Sleep(1);
-            }
+            Generator g = new Generator(new ConsoleLogger(LoggingType.All), numberOfCores, numberOfChunks, maxIterations, imageSize, numberOfColors, blackFinalColor);
+            ushort[] results = g.Generate(center, scaleSize);
 
             sw.Stop();
             Console.WriteLine("Took {0}ms.", sw.ElapsedMilliseconds);
             Console.WriteLine("Mandelbrot created, building image...");
 
-            // Create our colours.
-            Color[] colors = new Color[numberOfColors];
-            for (int i = 0; i < numberOfColors - 1; i++)
-            {
-                colors[i] = Color.FromArgb(255, 0, 0, (i + 1) * (256 / numberOfColors));
-            }
-
-            if (prompt.Prompt("Use black for max iteration color?", PromptOptions.Optional, 'Y', null, validateYesNo, charToUpper) == 'Y')
-            {
-                colors[numberOfColors - 1] = Color.FromArgb(255, 0, 0, 0);
-            }
-
             string filename = prompt.Prompt("Enter a file name to save as", PromptOptions.Optional, inputCenter.ToString() + "@" + scale + ".png");
-            // Create our image.
-            using (Bitmap image = new Bitmap(imageSize.Width, imageSize.Height))
-            {
-                for (int y = 0; y < imageSize.Height; y++)
-                {
-                    for (int x = 0; x < imageSize.Width; x++)
-                    {
-                        image.SetPixel(x, y, colors[results[pointToIndex(x, y)] / (int)(Math.Ceiling(maxIterations / (double)numberOfColors))]);
-                    }
-                }
 
-                image.Save(filename, System.Drawing.Imaging.ImageFormat.Png);
+            using (Bitmap image = g.BuildImage(results))
+            {
+                image.Save(filename, ImageFormat.Png);
             }
 
             Console.WriteLine("Image built.");
@@ -177,77 +114,6 @@ namespace Fractal_Generator
 
             Console.WriteLine("Press enter to quit...");
             Console.ReadLine();
-        }
-
-        public static Task<Result> GenerateSectionAsync(Chunk chunk, Point center, SizeF scaleSize, ushort maxIterations)
-        {
-            return Task.Run(() =>
-            {
-                return GenerateSection(chunk, center, scaleSize, maxIterations);
-            });
-        }
-
-        private static Result GenerateSection(Chunk chunk, Point center, SizeF scaleSize, ushort maxIterations)
-        {
-            int startRow = chunk.Start.Y;
-            int endRow = chunk.End.Y;
-
-            int startColumn = chunk.Start.X;
-            int endColumn = chunk.End.X;
-
-            int height = endRow - startRow;
-            int width = endColumn - startColumn;
-
-            ushort[] results = new ushort[height * width];
-
-            // We'll need all of these later.
-            int relativeRow = 0;
-            ushort iteration = 0;
-            double xTemp = 0;
-            double x0 = 0f;
-            double y0 = 0f;
-            double xL = 0f;
-            double yL = 0f;
-            double xSquared = 0;
-            double ySquared = 0;
-
-            double negCenterX = -center.X;
-            double negCenterY = -center.Y;
-            double reciprocalScaleSizeWidth = 1 / scaleSize.Width;
-            double reciprocalScaleSizeHeight = 1 / scaleSize.Height;
-
-            for (int y = startRow; y < endRow; y++)
-            {
-                for (int x = startColumn; x < endColumn; x++)
-                {
-                    // The formula for a mandelbrot is z = z^2 + c, basically. We must relate that in code.
-                    x0 = (x + negCenterX) * reciprocalScaleSizeWidth;
-                    y0 = (y + negCenterY) * reciprocalScaleSizeHeight;
-                    xL = 0;
-                    yL = 0;
-
-                    iteration = 0;
-                    xSquared = xL * xL;
-                    ySquared = yL * yL;
-                    xTemp = 0;
-
-                    while (xSquared + ySquared < twoSquared && iteration < maxIterations)
-                    {
-                        xTemp = xSquared - ySquared + x0;
-                        yL = 2 * xL * yL + y0;
-                        xL = xTemp;
-                        iteration++;
-                        xSquared = xL * xL;
-                        ySquared = yL * yL;
-                    }
-
-                    results[relativeRow * width + x] = iteration;
-                }
-
-                relativeRow++;
-            }
-
-            return new Result(chunk, results);
         }
     }
 }
